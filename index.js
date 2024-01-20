fc275ac3ef86108170fa1115fb53b1297ba64748
ahargunyllib <billy.bpm03@gmail.com>
2024-01-20 23:12:32 +0700

    integrating with mongodb

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dns = require('dns');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,6 +10,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ encoded: false }));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+});
+
+let Url = mongoose.model('shorturl', urlSchema);
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +32,24 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', async (req, res) => {
+  const countDocuments = await Url.countDocuments();
+  const original_url = req.body.url;
+  const short_url = countDocuments + 1;
+  
+  dns.lookup(original_url, (error, address, family) => {
+    if (error) {
+      res.json({
+        error: 'invalid url'
+      });
+    } else {
+      Url.findOne({
+        original_url: originalUrl
+      })
+    }
+  })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
